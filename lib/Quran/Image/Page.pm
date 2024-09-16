9bd246326d5620ff345d68dd965171853d852401
Ahmed El-Helw <ahmedre@gmail.com>
2024-09-16 20:52:48 +0400

    Integrate Abdullah's changes

--- lib/Quran/Image/Page.pm
+++ lib/Quran/Image/Page.pm
@@ -53,33 +53,79 @@ sub create {
 	my $fontfactor = 20.1;    # 21 is the default
 	my $fontdelta = 1; #(21 - abs(21 - $fontfactor)) / 21;
 
-   my @remainingPages = (291,323,336,345,347,349,423,446,455,457,521,526,567,577,585,586,597);
-   my %remainingHash = map {$_ => 1} @remainingPages;
+    my @pagesWith10 = (189);
+    my %pagesWith10Hash = map {$_ => 1} @pagesWith10;
 
-   my @pagesWith30 = (17,140,164,209,210,213,216);
-   my %pagesWith30Hash = map {$_ => 1} @pagesWith30;
+    my @pagesWith20 = (210,216,336);
+    my %pagesWith20Hash = map {$_ => 1} @pagesWith20;
 
-   my @pagesWith40 = (6,107,135,150,153,165,178,193,194,211,214,230,259,264,265,266,272,274,281);
-   my %pagesWith40Hash = map {$_ => 1} @pagesWith40;
+	my @pagesWith25 = (224,291,345,446,520,526,585);
+    my %pagesWith25Hash = map {$_ => 1} @pagesWith25;
+
+    my @pagesWith30 = (6,17,107,164,165,198,209,213,273,281,298,323,338,423,455,457,521,527,535,577,584,586,601);
+    my %pagesWith30Hash = map {$_ => 1} @pagesWith30;
+
+	my @pagesWith40 = (37,135,153,174,178,190,193,194,211,214,230,259,260,262,264,265,269,272,274,285,289,293,294,308,342,349,367,369,372,376,403,407,413,433,435,440,441,444,449,450,452,497,507,519,522,523,532,533,567,575,578,582,591,595,597);
+    my %pagesWith40Hash = map {$_ => 1} @pagesWith40;
+
+	my @pagesWith50 = (16,105,140,150,156,161,175,185,186,235,244,255,256,261,263,266,267,268,278,284,286,287,290,297,313,314,320,324,337,346,347,353,354,365,368,373,374,375,377,401,404,409,412,430,431,434,437,451,453,456,477,483,484,490,492,493,525,528,529,534,537,558,565,569,581,583,589,599);
+    my %pagesWith50Hash = map {$_ => 1} @pagesWith50;
+
+	my @pagesWith60 = (27,100,111,117,120,125,134,137,162,169,180,184,187,188,191,192,195,197,199,231,249,251,254,276,277,282,283,288,292,311,312,317,332,333,348,351,355,370,405,410,411,416,417,418,419,426,438,443,447,448,461,465,476,481,486,487,491,500,504,508,518,530,543,559,566,568,570,574,576,579,580,587,588,590,592,594);
+    my %pagesWith60Hash = map {$_ => 1} @pagesWith60;
+
+	my @pagesWith90 = (30,52,57,61,63,76,78,92,96,98,101,102,103,115,116,118,119,121,122,124,126,128,138,141,147,149,151,159,181,202,204,207,208,212,217,232,238,240,241,242,250,280,302,304,309,319,322,325,329,352,357,361,380,386,389,392,399,402,406,414,415,421,424,439,445,454,460,463,464,468,472,475,478,489,495,499,502,505,506,511,512,513,517,536,540,545,560,562,564,571,572,600);
+    my %pagesWith90Hash = map {$_ => 1} @pagesWith90;
+
+	my @pagesWith100 = (4,5,14,15,19,20,21,22,23,24,25,26,31,32,34,35,39,45,47,49,51,53,62,64,65,66,67,68,73,74,75,81,83,91,93,95,99,109,136,139,142,143,144,145,155,157,167,182,183,203,223,229,236,237,243,248,310,331,340,356,360,381,382,383,388,391,393,395,396,398,400,428,436,458,459,467,480,498,510,514,538,542,546,547,551,554,556,561,563,598,602,603);
+    my %pagesWith100Hash = map {$_ => 1} @pagesWith100;
+
+	my @pagesWith110 = (28,29,36,38,40,41,43,46,48,59,60,69,79,80,88,97,131,132,148,160,227,305,306,307,330,366,397,473,474,541,548,550,553,555,596,604);
+    my %pagesWith110Hash = map {$_ => 1} @pagesWith110;
+
+	my @pagesWith120 = (44,82,84,163,394,539,557);
+    my %pagesWith120Hash = map {$_ => 1} @pagesWith120;
+
+	my @pagesWith130 = (13);
+    my %pagesWith130Hash = map {$_ => 1} @pagesWith130;
+
+	my @pagesWith140 = (3);
+    my %pagesWith140Hash = map {$_ => 1} @pagesWith140;
 
 	# page 270 font is slightly larger so it goes off the page
-	if ($page->{number} == 270){
-		$fontfactor = 21.0;
-	} elsif ($page->{number} < 3) {
+	if ($page->{number} < 3) {
 		$fontfactor = 17.0;
-   } elsif ($page->{number} == 189) {
-      $fontfactor = 21.0;
-   } elsif ($page->{number} == 198) {
-      $fontfactor = 20.4;
-   } elsif ($page->{number} == 224) {
-      $fontfactor = 20.6;
-   } elsif (exists $pagesWith30Hash{$page->{number}}) {
-      $fontfactor = 20.4;
-   } elsif (exists $pagesWith40Hash{$page->{number}}) {
-      $fontfactor = 20.3;
-   } elsif (exists $remainingHash{$page->{number}}) {
-      $fontfactor = 20.4;
-   }
+	} elsif ($page->{number} == 270) {
+		$fontfactor = 20.5;
+	} elsif (exists $pagesWith10Hash{$page->{number}}) {
+        $fontfactor = 21.2;
+	} elsif (exists $pagesWith20Hash{$page->{number}}) {
+        $fontfactor = 21.1;
+	} elsif (exists $pagesWith25Hash{$page->{number}}) {
+		$fontfactor = 20.9;
+	} elsif (exists $pagesWith30Hash{$page->{number}}) {
+        $fontfactor = 20.7;
+	} elsif (exists $pagesWith40Hash{$page->{number}}) {
+		$fontfactor = 20.5;
+    } elsif (exists $pagesWith50Hash{$page->{number}}) {
+        $fontfactor = 20.4;
+	} elsif (exists $pagesWith60Hash{$page->{number}}) {
+        $fontfactor = 20.3;
+	} elsif (exists $pagesWith90Hash{$page->{number}}) {
+		$fontfactor = 19.9;
+	} elsif (exists $pagesWith100Hash{$page->{number}}) {
+		$fontfactor = 19.7;
+	} elsif (exists $pagesWith110Hash{$page->{number}}) {
+		$fontfactor = 19.6;
+	} elsif (exists $pagesWith120Hash{$page->{number}}) {
+		$fontfactor = 19.5;
+	} elsif (exists $pagesWith130Hash{$page->{number}}) {
+		$fontfactor = 19.3;
+	} elsif (exists $pagesWith140Hash{$page->{number}}) {
+		$fontfactor = 19.0;
+    } else {
+		$fontfactor = 20.1;
+	}
 
 	$page->{width}   = $self->{_width};
 	$page->{height}  = 6090; #$self->{_width} * Quran::Image::PHI * $fontdelta;
@@ -112,25 +158,40 @@ sub create {
 		$line->{box} = $self->_get_box($line);
 
 
-	   if ($page->{number} == 270){
-		   $page->{coord_y} += 120;
-      } elsif ($page->{number} < 3) {
-		   $page->{coord_y} += 136;
-      } elsif ($page->{number} == 189) {
-         $page->{coord_y} += 140;
-      } elsif ($page->{number} == 198) {
-         $page->{coord_y} += 134.5;
-      } elsif ($page->{number} == 224) {
-         $page->{coord_y} += 136.3;
-      } elsif (exists ($pagesWith30Hash{$page->{number}})) {
-         $page->{coord_y} += 134.5;
-      } elsif (exists ($pagesWith40Hash{$page->{number}})) {
-         $page->{coord_y} += 132;
-      } elsif (exists $remainingHash{$page->{number}}) {
-         $page->{coord_y} += 134;
-      } else {
-		   $page->{coord_y} += 130;
-      }
+	    if ($page->{number} < 3) {
+			$page->{coord_y} += 136;
+		} elsif ($page->{number} == 270) {
+			$page->{coord_y} += 116.5;
+		} elsif (exists ($pagesWith10Hash{$page->{number}})) {
+        	$page->{coord_y} += 143.75;
+		} elsif (exists ($pagesWith20Hash{$page->{number}})) {
+         	$page->{coord_y} += 142;
+		} elsif (exists $pagesWith25Hash{$page->{number}}) {
+			$page->{coord_y} += 140.25;
+		} elsif (exists ($pagesWith30Hash{$page->{number}})) {
+        	$page->{coord_y} += 138.25;
+		} elsif (exists ($pagesWith40Hash{$page->{number}})) {
+			$page->{coord_y} += 136.25;
+        } elsif (exists ($pagesWith50Hash{$page->{number}})) {
+        	$page->{coord_y} += 134.5;
+		} elsif (exists ($pagesWith60Hash{$page->{number}})) {
+        	$page->{coord_y} += 131.75;
+		} elsif (exists $pagesWith90Hash{$page->{number}}) {
+			$page->{coord_y} += 128;
+		} elsif (exists $pagesWith100Hash{$page->{number}}) {
+			$page->{coord_y} += 126.25;
+		} elsif (exists $pagesWith110Hash{$page->{number}}) {
+			$page->{coord_y} += 124.5;
+		} elsif (exists $pagesWith120Hash{$page->{number}}) {
+			$page->{coord_y} += 122.5;
+		} elsif (exists $pagesWith130Hash{$page->{number}}) {
+			$page->{coord_y} += 120.75;
+		} elsif (exists $pagesWith140Hash{$page->{number}}) {
+			$page->{coord_y} += 116;
+        } else {
+		    $page->{coord_y} += 130;
+        }
+
 		$page->{coord_y} -= $line->{box}->{min_y}
 		if $page->{coord_y} <= $page->{margin_top} and $line->{box}->{min_y} < 0;
 
